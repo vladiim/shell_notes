b733ce45e155fe2001eb509286f040e2b83fa2d7
vladiim <vladiim@yahoo.com.au>
2013-01-30 12:38:18 +1100

    Init + history.sh shortcuts

---
+++ history.sh
@@ -0,0 +1,14 @@
+cal 1222 1995
+# error
+^22
+# replace 1222 (error) w/ 22
+
+ls /user/local/src
+# last command
+!!/dir
+# repeat last command w/ /dir added
+cp !$/somefile
+# replace last command w/ cp and add somefile
+
+!ca # + tab
+# complete last command starting w/ ca in this case cal...
