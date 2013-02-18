7bfbfa660023fca21105765487b3955858c3adb8
vladiim <vladiim@yahoo.com.au>
2013-02-18 12:34:49 +1100

    More history

--- notes/history.sh
+++ notes/history.sh
@@ -1,4 +1,11 @@
+# list history
+history
+
 # ! ;
+
+history # lists history
+
+history -40 -20 # history between 40 and 20
 
 cal 1222 1995
 # error
@@ -6,9 +13,16 @@ cal 1222 1995
 # replace 1222 (error) w/ 22
 
 ls /user/local/src
+
 # last command
+!! # repeat the last command
+
 !!/dir
 # repeat last command w/ /dir added
+
+!-1 # command before last
+!-3 # 3 commands before last
+
 cp !$/somefile
 # replace last command w/ cp and add somefile
 
@@ -25,4 +39,42 @@ Esc>	# end of command history
 Ctrl-r  # search backwards for the terms i've typed
 
 Ctrl-s  # search forward for the terms i've typed
+
+# HISTORY WORD RETRIEVAL
+
+# numbers - 0 is the commandline, 1 is the first arg
+# $ = last arg
+# 2 numbers with - in between = range
+
+echo blah blah foo
+!!:0               #=> echo
+!!:1               #=> blah blah foo
+!!:0-1             #=> echo blah
+echo I said !!:$   #=> echo I said foo
+echo the args !!:* #=> echo the args blah blah foo
+
+# SUBSTITUTION
+
+echo blah foo bar baz
+!!:s/foo/lolz
+echo blah lolz bar baz
+
+# repeat with &
+echo johnny foo smith
+!!:&
+echo johnny lolz smith
+
+# short cut
+echo blah foo bar baz
+^foo bar^lolz
+echo blah lolz baz
+
+# global
+echo blah foo bar baz foo
+!!:gs/foo/lolz
+echo blah lolz bar baz lolz
+
+# vars
+foo='two words'
+echo ${foo:s/two words/oneword}
 
